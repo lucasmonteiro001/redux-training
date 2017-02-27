06915663d573ab83761fe7e2610bad035433050c
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-02-27 11:07:59 -0300

    BookList container added

---
+++ src/containers/book-list.js
@@ -0,0 +1,36 @@
+/**
+ * Created by lucas on 2/27/17.
+ */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class BookList extends Component {
+
+    renderList = () => {
+        return this.props.books.map((book) => {
+            return (
+                <li key={book.title} className="list-group-item">
+                    {book.title}
+                </li>
+            )
+        });
+    };
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    // Whatever is returnet will show up as props
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by lucas on 2/27/17.
+ */
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
